07f24ce6dbf97ceb92d46119bc51762a2fd19a46
Christopher Slater <chris@fourzerofour.uk>
2016-05-01 10:03:07 +0100

    Move to environment variables

--- src/env.js
+++ src/env.js
@@ -1,15 +1,18 @@
-import path from 'path';
-const env = process.env.NODE_ENV || 'development';
-let returnValue;
+// import path from 'path';
+// const env = process.env.NODE_ENV || 'development';
+// let returnValue;
+//
+// if (__CLIENT__) {
+//  if (env === 'production') {
+//    returnValue = require('env-production');
+//  } else {
+//    returnValue = require('env-development');
+//  }
+// } else {
+//  returnValue = require(path.resolve('envConfig', `env-${env}`));
+// }
 
-if (__CLIENT__) {
-  if (env === 'production') {
-    returnValue = require('env-production');
-  } else {
-    returnValue = require('env-development');
-  }
-} else {
-  returnValue = require(path.resolve('envConfig', `env-${env}`));
-}
-
-export default returnValue;
+export default {
+  CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
+  CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+};
